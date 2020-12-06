943f3e7708df2e1eff93bef2e92c8e8a94211de5
Edgar Rivas <webdevedd@gmail.com>
2020-12-06 02:40:17 -0800

    removed all js

--- app.js
+++
@@ -1,6 +0,0 @@
-//SMOOTH SCROLL FUNCTION
-function smoothScroll(target){
-    var target = document.querySelector(target);
-    var targetPosition = target.offsetTop;
-    window.scrollTo(0, targetPosition);
-}
