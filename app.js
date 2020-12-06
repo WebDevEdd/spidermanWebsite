a7391b723b493cffa10cef37c6f86003975c14c2
Edgar Rivas <webdevedd@gmail.com>
2020-12-06 02:21:09 -0800

    commit test for github

--- app.js
+++ app.js
@@ -4,12 +4,3 @@ function smoothScroll(target){
     var targetPosition = target.offsetTop;
     window.scrollTo(0, targetPosition);
 }
-
-var castSection = document.querySelector('.cast-btn');
-castSection.addEventListener('click', function(){
-    smoothScroll('.cast');
-})
-var mediaSection = document.getElementById('mediaButton');
-mediaSection.addEventListener('click', function(){
-    smoothScroll('.media');
-})
